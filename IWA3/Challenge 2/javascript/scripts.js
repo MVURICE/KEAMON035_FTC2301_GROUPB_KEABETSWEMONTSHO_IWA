fbcb0bbad5c71e10fabca7580328bfaa4e5bdc8e
MVURICE <mauricemontsgo@gmail.com>
2023-03-30 13:44:09 +0200

    added IWA4 file

--- IWA3/Challenge 2/javascript/scripts.js
+++ IWA3/Challenge 2/javascript/scripts.js
@@ -1,4 +1,5 @@
 import { role as johanesrole } from "./johannes.js";
 import { role as alexrole} from "./alex.js";
 import { role as nwabisarole } from "./nwabisa.js";
+
 console.log('Roles:', nwabisarole, johanesrole, alexrole)
